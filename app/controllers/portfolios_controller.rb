b467c88d78ac80be2da04ce7e3fc7f65f10c2566
anamihiczunic <anamihiczunic@gmail.com>
2021-04-15 10:29:46 +0200

    Created seeds file for sample ddata

---
+++ app/controllers/portfolios_controller.rb
@@ -0,0 +1,25 @@
+class PortfoliosController < ApplicationController
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+end
