606a3d6f6e40d5ae6dbdb9efe35b6396543d5a24
anamihiczunic <anamihiczunic@gmail.com>
2021-04-28 10:42:35 +0200

    Integrated new gem into rails app for copyright

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,11 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
-end
 
+  before_action :set_copy_right
+
+  def set_copy_right
+    @copyright = AnamViewTool::Rendered.copyright 'Ana Mihic Zunic', 'All rights reserved'
+  end
+
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    AnamViewTool::Rendered.copyright 'Ana Mihic Zunic', 'All rights reserved'
+  end
 end
