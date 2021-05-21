a2dcf59999024230ceb9757cc30242101a70f7b9
anamihiczunic <anamihiczunic@gmail.com>
2021-05-21 12:23:08 +0200

    Implemented authorization engine for portfolio items and blogs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,4 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
-
-  before_action :set_copy_right
-
-  def set_copy_right
-    @copyright = AnamViewTool::Rendered.copyright 'Ana Mihic Zunic', 'All rights reserved'
-  end
-
 end
