a805b5a39a0c6c87d509abbba867849fe3dd7bfb
anamihiczunic <anamihiczunic@gmail.com>
2021-06-12 13:23:39 +0200

    Added validations for topics in blogs and added a topic form element

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,8 +3,8 @@ class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  validates_presence_of :title, :body
-  belongs_to :topic, optional: true
+  validates_presence_of :title, :body, :topic_id
+  belongs_to :topic
   has_many :comments, dependent: :destroy
 
   def self.special_blogs
